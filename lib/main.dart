f2780e8ddd64ec13d97714635222ea99f9355477
wyznu <wiz51ceae@gmail.com>
2021-05-31 06:03:47 +0700

    penambahan halaman

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'color_bloc.dart';
+import 'page2.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: BlocProvider<ColorBloc>(
           create: (context) => ColorBloc(Colors.green), child: MainPage()),
     );
@@ -19,34 +21,48 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       floatingActionButton:
           Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
         FloatingActionButton(
             backgroundColor: Colors.green,
-            onPressed: () => context.read<ColorBloc>().add(ColorEvent.to_green)),
+            onPressed: () =>
+                context.read<ColorBloc>().add(ColorEvent.to_green)),
         SizedBox(
           width: 10,
         ),
         FloatingActionButton(
             backgroundColor: Colors.purple,
-            onPressed: () => context
-                .read<ColorBloc>()
-                .add(ColorEvent.to_purple))
+            onPressed: () =>
+                context.read<ColorBloc>().add(ColorEvent.to_purple))
       ]),
       appBar: AppBar(
         title: Center(child: Text('tes bloc')),
       ),
       body: Center(
-        child: BlocBuilder<ColorBloc, Color>(
-          builder: (context, currentColor) => AnimatedContainer(
-            width: 300,
-            height: 400,
-            color: currentColor,
-            duration: Duration(seconds: 3),
-          ),
-        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<ColorBloc, Color>(
+                builder: (context, currentColor) => AnimatedContainer(
+                  width: 300,
+                  height: 400,
+                  color: currentColor,
+                  duration: Duration(seconds: 3),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Page2();
+                    }));
+                  },
+                  child: Text('halaman 2'))
+            ]),
       ),
     );
   }
---
+++ lib/page2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('halaman 2')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(onPressed: () {
+                Navigator.pop(context);
+              }, child: Text('kembali'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
