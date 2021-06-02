887d8b716cedcafb09417f0070d91fd91283dd29
wyznu <wiz51ceae@gmail.com>
2021-06-03 05:44:04 +0700

    update tombol

--- lib/main.dart
+++ lib/main.dart
@@ -22,47 +22,46 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-        FloatingActionButton(
-            backgroundColor: Colors.green,
-            onPressed: () =>
-                context.read<ColorBloc>().add(ColorEvent.to_green)),
-        SizedBox(
-          width: 10,
-        ),
-        FloatingActionButton(
-            backgroundColor: Colors.purple,
-            onPressed: () =>
-                context.read<ColorBloc>().add(ColorEvent.to_purple))
-      ]),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          child: Icon(Icons.exit_to_app),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return Page2();
+            }));
+          }),
       appBar: AppBar(
         title: Center(child: Text('tes bloc')),
       ),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              BlocBuilder<ColorBloc, Color>(
-                builder: (context, color) => AnimatedContainer(
-                  width: 250,
-                  height: 320,
-                  color: color,
-                  duration: Duration(seconds: 3),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Page2();
-                    }));
-                  },
-                  child: Text('halaman 2'))
-            ]),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          BlocBuilder<ColorBloc, Color>(
+            builder: (context, color) => AnimatedContainer(
+              width: 250,
+              height: 320,
+              color: color,
+              duration: Duration(seconds: 3),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                  onTap: () =>
+                      context.read<ColorBloc>().add(ColorEvent.to_purple),
+                  child:
+                      Container(color: Colors.purple, width: 40, height: 40)),
+              GestureDetector(
+                  onTap: () =>
+                      context.read<ColorBloc>().add(ColorEvent.to_green),
+                  child: Container(color: Colors.green, width: 40, height: 40)),
+            ],
+          ),
+        ]),
       ),
     );
   }
